a80dc0f5be8c54eb2bf8c12ae7f83170d4e8fbfc
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2024-05-04 22:03:30 +0530

    Making Dynamic

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React , { useEffect, useState } from 'react'
+import moment from 'moment'
 
 import './Feed.css'
 
@@ -12,106 +13,33 @@ import thumbnail7 from '../../assets/thumbnail7.webp'
 import thumbnail8 from '../../assets/thumbnail8.jfif'
 
 import { Link } from 'react-router-dom'
+import { API_KEY, value_converter } from '../../data'
 
-function Feed() {
+function Feed({category}) {
+
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+        await fetch(videoList_url).then(response=> response.json()).then(data => setData(data.items))
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[category])
+
     return(
         <div className="feed">
-            <Link to={`video/34/7485`} className="card">
-                <img src={thumbnail1} alt="" />
-                <h2>Top 5 phones to buy in April 2024</h2>
-                <h3>MernStatue</h3>
-                <p>16M views &bull; 17 days ago </p>
-            </Link>
-            <div className="card">
-                <img src={thumbnail2} alt="" />
-                <h2>How to motivate yourself</h2>
-                <h3>Motivate</h3>
-                <p>1.6M views &bull; 5 days ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail3} alt="" />
-                <h2>Top 5 fastest cars </h2>
-                <h3>Running Vehices</h3>
-                <p>198k views &bull; 7 months ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail4} alt="" />
-                <h2>Play this new Amazing game</h2>
-                <h3>Tech Gaming</h3>
-                <p>60k views &bull; 15 days ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail5} alt="" />
-                <h2>Big trillon days</h2>
-                <h3>Share News</h3>
-                <p>80k views &bull; 21 hours ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail6} alt="" />
-                <h2>Chotu Lambu episode 3</h2>
-                <h3>Cartoon Adda</h3>
-                <p>80M views &bull; 1 year ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail7} alt="" />
-                <h2>Everything about Discipline</h2>
-                <h3>Discipline</h3>
-                <p>160k views &bull; 3 months ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail8} alt="" />
-                <h2>200 Push Up challenge</h2>
-                <h3>Daily Workout</h3>
-                <p>140k views &bull; 4 years ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail3} alt="" />
-                <h2>Top 5 fastest cars </h2>
-                <h3>Running Vehices</h3>
-                <p>198k views &bull; 7 months ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail4} alt="" />
-                <h2>Play this new Amazing game</h2>
-                <h3>Tech Gaming</h3>
-                <p>60k views &bull; 15 days ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail5} alt="" />
-                <h2>Big trillon days</h2>
-                <h3>Share News</h3>
-                <p>80k views &bull; 21 hours ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail6} alt="" />
-                <h2>Chotu Lambu episode 3</h2>
-                <h3>Cartoon Adda</h3>
-                <p>80M views &bull; 1 year ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail5} alt="" />
-                <h2>Big trillon days</h2>
-                <h3>Share News</h3>
-                <p>80k views &bull; 21 hours ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail6} alt="" />
-                <h2>Chotu Lambu episode 3</h2>
-                <h3>Cartoon Adda</h3>
-                <p>80M views &bull; 1 year ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail7} alt="" />
-                <h2>Everything about Discipline</h2>
-                <h3>Discipline</h3>
-                <p>160k views &bull; 3 months ago </p>
-            </div>
-            <div className="card">
-                <img src={thumbnail8} alt="" />
-                <h2>200 Push Up challenge</h2>
-                <h3>Daily Workout</h3>
-                <p>140k views &bull; 4 years ago</p>
-            </div>
+            {data.map((item, index) => {
+                return(
+                    <Link to={`video/${item.snippet.categoryId}/${item.id}`} className="card">
+                        <img src={item.snippet.thumbnails.medium.url} alt="" />
+                        <h2>{item.snippet.title}</h2>
+                        <h3>{item.snippet.channelTitle}</h3>
+                        <p>{value_converter(item.statistics.viewCount)} views &bull; {moment(item.snippet.publishedAt).fromNow()} </p>
+                    </Link>
+                )
+            })}
         </div>
     );
 }
--- src/data.js
+++ src/data.js
@@ -1,1 +1,13 @@
 export const API_KEY = 'AIzaSyDO8j59yQ_vTY5xGqhjLzmIxOxvgQVh7M4';
+
+export const value_converter = (value) => {
+    if(value >= 1000000) {
+        return Math.floor(value/1000000) + "M";
+    }
+    else if(value >= 1000) {
+        return Math.floor(value/1000) + "K";
+    }
+    else {
+        value;
+    }
+}
